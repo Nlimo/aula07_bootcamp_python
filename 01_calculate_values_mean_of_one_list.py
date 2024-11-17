f83cb30e4311aa5523157975e481821cbd0a1278
Nlimo <lucaswilliamschaves@gmail.com>
2024-11-17 11:30:54 -0300

    add file and code of the quest 01, function values mean of one list

---
+++ 01_calculate_values_mean_of_one_list.py
@@ -0,0 +1,8 @@
+
+lista: list = [1, 2, 3, 4, 5]
+
+def calculate_values_mean_of_one_list(list):
+    result = sum(list) / len(list)
+    return  print(result)
+
+calculate_values_mean_of_one_list(lista)
